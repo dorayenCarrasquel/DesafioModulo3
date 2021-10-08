f247f4700837563a46e562787d4cfd60123910ba
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-08 13:12:57 -0300

    Cadastrar vendedor no Sistema

---
+++ src/br/com/zup/ServiceVendedor.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceVendedor {
+    private static List<Vendedor> vendedores = new ArrayList<>();
+
+    public static Vendedor cadastrarVendedor(String nome, String email, String cpf){
+        Vendedor vendedorCadastrado = new Vendedor(nome, email, cpf);
+        vendedores.add(vendedorCadastrado);
+        return vendedorCadastrado;
+    }
+}

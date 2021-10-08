c5ee6bdc95c30bc7cc2818b00b80b4d557de18d6
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-08 15:11:02 -0300

    Revert "Testar cadastros na Main"
    This reverts commit cde3ad1719589a6170ae4a6e2ee9481e56030c31.

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,14 +3,8 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
+        Cliente clienteCadastro= Sistema.cadastrarCliente();
+        Cliente clienteCadastrO= Sistema.cadastrarCliente();
         System.out.println(ServiceCliente.clientes);
-        Cliente clienteCadastro = Sistema.cadastrarCliente();
-        Cliente clienteCadastrO = Sistema.cadastrarCliente();
-        System.out.println(ServiceCliente.clientes);
-
-        Vendedor vendedorCada = Sistema.cadastrarVendedor();
-        Vendedor vendedorCada1 = Sistema.cadastrarVendedor();
-        System.out.println(ServiceVendedor.vendedores);
-
     }
 }
--- src/br/com/zup/ServiceVendedor.java
+++ src/br/com/zup/ServiceVendedor.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceVendedor {
-    public static List<Vendedor> vendedores = new ArrayList<>();
+    private static List<Vendedor> vendedores = new ArrayList<>();
 
     public static Vendedor cadastrarVendedor(String nome, String email, String cpf){
         Vendedor vendedorCadastrado = new Vendedor(nome, email, cpf);
